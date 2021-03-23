ebe3baf6d5e69b55601714b632e0680bd201c10c
Peng <pli9@bwh.harvard.edu>
2021-03-23 05:08:58 -0400

    publish dirNode.m

---
+++ dirNode.m
@@ -0,0 +1,24 @@
+function dirNode(treeHandle, topFolder, varargin)
+% DIRNODE generate parent node and walk subfolders (1 level) to create 
+%    child nodes to parent node treeHandle
+% 
+%   VARARGIN
+%       context menu: Open
+% 
+% $Author: Peng Li
+% $Date:   Mar. 22, 2021
+% 
+
+% root node
+[~, f] = fileparts(topFolder);
+rnode  = uitreenode(treeHandle, 'Text', f, 'Icon', 'folderClosed.gif');
+
+nodeData.Path  = topFolder;
+rnode.NodeData = nodeData;
+
+switch nargin
+    case 3
+        rnode.ContextMenu = varargin{1};
+end
+
+intoFolder(rnode, string(topFolder), 0, varargin{:});
